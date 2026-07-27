0ab2c056daa68a178db616601e0e83d2be9828bb
Raheem Elnashar <fastfast415@gmail.com>
2026-07-27 17:05:10 +0300

    bind services dynamically based on routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Contracts\DegreesOCRInterface;
+use App\services\DegreesOCR\AiPrompt;
+use App\services\DegreesOCR\GeminiService;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +15,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(DegreesOCRInterface::class,function ($app){
+            if (request()->routeIs('Gemini')) {
+                return $app->make(GeminiService::class);
+            }
+            if (request()->routeIs('Prompt')) {
+                return $app->make(AiPrompt::class);
+            }
+            
+        } );
     }
 
     /**
